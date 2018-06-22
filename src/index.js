dfb637be387c8044e0b12f6aee5a31c3d9ad840c
[Derek] <[derekgc@icloud.com]>
2018-06-21 20:55:44 -0400

    functioning done

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ var singleArray = random.slice(0,8);
 var doubleArray = singleArray;
 singleArray.forEach(e => doubleArray.push(e));
 
-
-
-
 var shuffledArray = shuffle(doubleArray);
 
 
@@ -18,55 +15,86 @@ let gamePad = document.getElementById("pokemon-container");
 let userClick = document.getElementById("container");
 
 
-console.log(gamePad);
+// console.log(gamePad);
 // debugger;
 
 document.addEventListener("DOMContentLoaded", function() {
   let diff = 16;
-  let revealed;
+  let revealed = null;
+  let finished =0;
 
   // debugger;
 
   for(let i = 0; i < diff; i++){
-
-    newText = document.createElement('h1');
-    newText.innerText = "?";
-    newText.style = "font-size: 90px";
-    newText.id = shuffledArray[i];
-
-    newPoke = document.createElement('div');
-    newPoke.className = "pokemon-container";
-
-    newLetter = document.createElement('div');
-
-    newLetter.className = "pokemon-frame center-text";
-    newLetter.style = "width:230px;margin:10px;background:#fecd2f;color:#2d72fc";
-
-    newLetter.appendChild(newText);
-    newPoke.appendChild(newLetter);
-    gamePad.appendChild(newPoke);
-
+    appendToPad(shuffledArray[i]);
   }
 
 
 userClick.addEventListener('click', event => {
-  if (event.target.localName === 'h1') {
+  if (event.target.localName === 'h1' && event.target.dataset.fliped == 0) {
 
     if(event.target.innerText !=='?'){
-      revealed = event.target.innerText;
       event.target.innerText ='?';
     }else {
       event.target.innerText = event.target.id;
     }
-  }
-  console.log(event);
+    if(revealed === null) {
+      revealed = event.target;
+    }else {
+      if(revealed !== event.target && revealed.innerText === event.target.innerText){
+        revealed.dataset.fliped =1;
+        event.target.dataset.fliped =1;
+        finished = finished +2;
+        //mark both done;
+        console.log(finished);
+      }
+      revealed = null;
+      setTimeout(flipAllBack, 500);
 
+    }
+
+    if(finished >= diff){
+      alert("You Won!");
+    }
+
+
+  }//if (event.target.localName === 'h1') {
+  // console.log(event);
+
+
+
+}); //userClick.addEventListener
 
 
 });
 
+function flipAllBack() {
+  const allTexts = document.getElementsByTagName("H1");
+  [...allTexts].forEach(e => {
+    if(e.dataset.fliped == 0) e.innerText = "?";
+  });
+}
 
-})
+function appendToPad(id){
+  newText = document.createElement('h1');
+  newText.innerText = "?";
+  newText.style = "font-size: 90px";
+  newText.id = id;
+  newText.dataset.fliped = 0;
+
+  newPoke = document.createElement('div');
+  newPoke.className = "pokemon-container";
+
+  newLetter = document.createElement('div');
+
+  newLetter.className = "pokemon-frame center-text";
+  newLetter.style = "width:230px;margin:10px;background:#fecd2f;color:#2d72fc";
+
+  newLetter.appendChild(newText);
+  newPoke.appendChild(newLetter);
+  gamePad.appendChild(newPoke);
+
+}
 
 
 function shuffle(a) {
